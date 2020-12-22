720bcb8dc8752037a768aae1fd1491bead77dacf
jomat18 <manu.jose.torres@gmail.com>
2020-12-22 01:31:01 -0500

    feat: pruebas locales generando thumbnails

---
+++ local-test/index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const { spawnSync, exec } = require('child_process');
+const { readFileSync, writeFileSync, unlinkSync } = require('fs');
+const path = require('path')
+
+const ffprobePath = path.resolve(__dirname, 'ffmpeg-static', 'ffprobe')
+const ffmpegPath = path.resolve(__dirname, 'ffmpeg-static', 'ffmpeg')
+
+const srcKey = 'prueba.mp4'
+const width = 480
+const height = -1  
+const time = 60 //seconds
+
+// ffmpeg -i prueba.mp4 -f image2 -vf "select='eq(pict_type,PICT_TYPE_I)', scale=480:-1" -vsync vfr out%03d.png -v quiet -stats 2> result.txt
+
+exec("rm tmp/*", function (error, stdout, stderr) {
+	if(error)
+     	console.log('Directory Empty', error);
+  	else
+    	console.log("Files Deleted");
+});
+
+
+function createThumbnail(seek, x) {
+      
+      const ffmpeg = spawnSync(ffmpegPath, [
+        '-i', srcKey, 
+        '-ss', seek,      
+        '-vf', `thumbnail,scale=${width}:${height}`,
+        '-qscale:v', '2',
+        '-vframes', '1',
+        '-f', 'image2',
+        '-c:v', 'mjpeg',
+        `tmp/${x}.jpg`
+      ])      
+  }
+
+//function uploadThumbnail() {
+
+//}
+
+
+  var ffprobe = spawnSync(ffprobePath, [
+    '-v',
+    'error',
+    '-show_entries',
+    'format=duration, filename',
+    '-of',
+    'default=nw=1:nk=1',
+    //'-sexagesimal',
+    srcKey
+  ])
+
+  const values = ffprobe.stdout.toString().split('\n')
+  console.log(values)
+  const duration = Math.ceil(values[1])
+  console.log(duration);
+
+  var ffprobeArgs = [
+	'-v',
+    'error',
+    '-show_entries',
+    'format_tags=comment',
+    '-of',
+    'default=nw=1:nk=1',
+    srcKey
+  	]
+
+ffprobe = spawnSync(ffprobePath, ffprobeArgs)
+
+console.log(ffprobe.stdout.toString('utf8').split('\n')[0])  
+
+var i = 1;
+  for(var s = 0; s < duration; s+=time){
+    createThumbnail(s, i);
+	//uploadThumbnail(i);
+  	i++;
+  }
+
+
+/*
+function show_files() {
+	const testFolder = 'tmp/';
+
+	fs.readdir(testFolder, (err, files) => {
+	  files.forEach(file => {
+	    console.log(file);
+	  });
+	});	
+}
+*/
