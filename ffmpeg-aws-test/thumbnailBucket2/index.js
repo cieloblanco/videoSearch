51d6528d7a31046cc368f506eded462d070e1eae
jomat18 <manu.jose.torres@gmail.com>
2020-12-23 17:46:27 -0500

    test : probando cambios

--- ffmpeg-aws-test/thumbnailBucket2/index.js
+++ ffmpeg-aws-test/thumbnailBucket2/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const AWS = require('aws-sdk')
-const { spawn, spawnSync, exec } = require('child_process')
-const { readFileSync, writeFileSync, unlinkSync, readdir } = require('fs');
+const { spawnSync, exec } = require('child_process')
+const { readFileSync, writeFileSync, unlinkSync } = require('fs');
 
 const ffprobePath = '/opt/ffmpeg-static/ffprobe';
 const ffmpegPath = '/opt/ffmpeg-static/ffmpeg';
@@ -10,7 +10,7 @@ const ffmpegPath = '/opt/ffmpeg-static/ffmpeg';
 const allowedTypes = ['mov', 'mpg', 'mpeg', 'mp4', 'wmv', 'avi', 'webm']
 const width = 480
 const height = -1
-const time = 60 //seconds
+const interval = 14 //seconds
 
 // get reference to S3 client
 const s3 = new AWS.S3()
@@ -33,33 +33,26 @@ module.exports.handler = async (event, context) => {
     throw new Error(`filetype: ${fileType} is not an allowed type`)
   }
 
-// ffmpeg -i prueba.mp4 -f image2 -vf "select='eq(pict_type,PICT_TYPE_I)', scale=480:-1" -vsync vfr out%03d.png -v quiet -stats 2> result.txt
+  // ffmpeg -i prueba.mp4 -f image2 -vf "select='eq(pict_type,PICT_TYPE_I)', scale=480:-1" -vsync vfr out%03d.png -v quiet -stats 2> result.txt
 
-exec("rm /tmp/*", function (error, stdout, stderr) {
-	if(error)
-     	console.log('Directory Empty', error);
-  	else
-    	console.log("Files Deleted");
-});
+  try {
 
-try {
-
-	const params = {
+	  const params = {
 		            Bucket: srcBucket,
 		            Key: srcKey
 		        };
-	// get the video    
-	const video = await s3.getObject(params).promise();
+	  // get the video    
+	  const video = await s3.getObject(params).promise();
 
-	// write the file to disk
-	writeFileSync(`/tmp/${srcKey}`, video.Body);
+	  // write the file to disk
+	  writeFileSync(`/tmp/${srcKey}`, video.Body);
 
-} catch (error) {
+  } catch (error) {
     console.log(error);
     return;
-} 
+  } 
 
-function createThumbnail(seek, x) {
+  function createThumbnail(seek, x) {
       const ffmpeg = spawnSync(ffmpegPath, [
         '-i', `/tmp/${srcKey}`,
         '-ss', seek,      
@@ -72,24 +65,23 @@ function createThumbnail(seek, x) {
       ])      
   }
 
-function uploadThumbnail(x) {
-	return new Promise((resolve, reject) => {
+  function uploadThumbnail(x, minutes, seconds) {
+	  return new Promise((resolve, reject) => {
 	
-    const image = readFileSync(`/tmp/${x}.jpg`);
+      const image = readFileSync(`/tmp/${x}.jpg`);
 
-    unlinkSync(`/tmp/${x}.jpg`);
+      unlinkSync(`/tmp/${x}.jpg`);
 
-	let dstKey = `${x}.jpg`;
+	    let dstKey = `${x}_${minutes}_${seconds}.jpg`;
 
-	const destparams = {
+	    const destparams = {
         Bucket: thumbnailBucket,
         Key: dstKey,
         Body: image,
         ContentType: "image"
-        //ContentType: `image/jpg`
       }
 
-	s3.upload(destparams, function(err, data) {
+	    s3.upload(destparams, function(err, data) {
         if (err) {
           console.log(err)
           reject()
@@ -98,57 +90,38 @@ function uploadThumbnail(x) {
         resolve()
       })
     })
-}
+  }
 
 
   var ffprobe = spawnSync(ffprobePath, [
     '-v',
     'error',
     '-show_entries',
-    'format=duration, filename',
+    'format=duration',
     '-of',
     'default=nw=1:nk=1',
-    //'-sexagesimal',
     `/tmp/${srcKey}`,
   ])
 
-  const values = ffprobe.stdout.toString().split('\n')
-  console.log(values)
-  const duration = Math.ceil(values[1])
-  console.log(duration);
+  const result = ffprobe.stdout.toString()
+  const duration = Math.ceil(result)
 
-  var ffprobeArgs = [
-	'-v',
-    'error',
-    '-show_entries',
-    'format_tags=comment',
-    '-of',
-    'default=nw=1:nk=1',
-    `/tmp/${srcKey}`,
-  	]
+  var minutes = 0;
+  var seconds = 0;
+  var i = 1;
 
-ffprobe = spawnSync(ffprobePath, ffprobeArgs)
+  for(var s = interval; s < duration; s+=interval) {
+    seconds = s;
+    minutes = Math.trunc(s/60);
 
-console.log(ffprobe.stdout.toString('utf8').split('\n')[0])  
+    if (s>=60) {
+      seconds = s%60;
+    }
 
-var i = 1;
-  for(var s = 0; s < duration; s+=time){
     createThumbnail(s, i);
-	  await uploadThumbnail(i);
-  	i++;
-  }
+    await uploadThumbnail(i, minutes, seconds);
+	  i++;
+  } 
 
   unlinkSync(`/tmp/${srcKey}`);
 }
-
-/*
-function show_files() {
-	const testFolder = '/tmp/';
-
-	readdir(testFolder, (err, files) => {
-	  files.forEach(file => {
-	    console.log(file);
-	  });
-	});	
-}
-*/
